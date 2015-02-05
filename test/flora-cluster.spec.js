6afd343851fa21a5711fdae4b72e7078927bcd16
Nico Kaiser <nico.kaiser@boerse-go.de>
2015-02-05 18:44:05 +0100

    Add initial test folder

---
+++ test/flora-cluster.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+var floraCluster = require('../');
+
+describe('flora-cluster', function () {
+    it('should be an object', function () {
+        expect(floraCluster).to.be.an('object');
+    });
+});
