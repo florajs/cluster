890aaddbf06a41a7daa2cec0b1f6a6f1a4a1cc78
Nico Kaiser <nico.kaiser@boerse-go.de>
2015-02-06 10:17:23 +0100

    Add some interface tests for flora-cluster

--- test/flora-cluster.spec.js
+++ test/flora-cluster.spec.js
@@ -8,4 +8,22 @@ describe('flora-cluster', function () {
     it('should be an object', function () {
         expect(floraCluster).to.be.an('object');
     });
+
+    it('should expose master and worker objects', function () {
+        expect(floraCluster).to.have.property('master');
+        expect(floraCluster).to.have.property('worker');
+    });
+
+    describe('worker', function () {
+        it('should be an object', function () {
+            expect(floraCluster.worker).to.be.an('object');
+        });
+
+        it('should expose functions', function () {
+            expect(floraCluster.worker).to.have.property('run');
+            expect(floraCluster.worker).to.have.property('attach');
+            expect(floraCluster.worker).to.have.property('serverStatus');
+            expect(floraCluster.worker).to.have.property('shutdown');
+        });
+    });
 });
