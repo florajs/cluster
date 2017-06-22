15a759af0177378f98cbad03659a083bce01495c
Marco Baumgartl <marco.baumgartl@boerse-go.de>
2017-06-22 10:59:08 +0200

    Allow setting status items by object
    Avoids consecutive calls if multiple items set at once. Instead of
    
    status.set('key1', 'val1');
    status.set('key2', 'val2');
    
    it's shorter to write:
    
    status.set({ key1: 'val1', key2: 'val2' });

---
+++ test/status.spec.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { expect } = require('chai');
+
+const Status = require('../lib/status');
+
+describe.only('status', () => {
+    let status;
+
+    beforeEach(() => status = new Status());
+
+    describe('set', () => {
+        it('should support key/value', () => {
+            status.set('key', 'val');
+            expect(status.getStatus()).to.have.property('key', 'val');
+        });
+
+        it('should support objects', () => {
+            status.set({ key1: 'val1', key2: 'val2' });
+            expect(status.getStatus()).to.include({ key1: 'val1', key2: 'val2' });
+        });
+    });
+});
